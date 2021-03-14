30a132946caecba62eaac572e2537834d745f16e
Basheir Elbashier <elbashier97@gmail.com>
2021-03-14 14:56:41 +0200

    [MOD] modify models/database.php add model T_EVENT because it is needed

--- MODEL/database.php
+++ MODEL/database.php
@@ -27,7 +27,28 @@ $full_name = 'الآدمن';
 $query = "INSERT INTO T_USERS VALUES (1,'admin','" . md5("admin") . "',True,'" . $full_name . "',1,CURTIME(),'')";
 mysqli_query($connect, $query) or die("");
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
+$query = "CREATE TABLE T_EVENT (
+    ID INT(20),
+    EVENT_TYPE VARCHAR(40),
+    CLASSIFICATION VARCHAR(1),
+    EVENT_NAME VARCHAR(60),
+    ORGANIZER VARCHAR(40),
+    EVENT_LOCATION VARCHAR(40),
+    EXPECTED_AUDIENCE INT(8),
+    POLICE_COUNT INT(8),
+    EVENT_DATE DATETIME,
+    EVENT_DAY VARCHAR(10),
+    DATE_CREATED DATETIME,
+    CREATOR_ID INT(3),
+    DATE_MODIFIED DATETIME,
+    MODIFIER_ID INT(3),
+    FOREIGN KEY (CREATOR_ID) REFERENCES T_USERS(USER_NO),
+    FOREIGN KEY (MODIFIER_ID) REFERENCES T_USERS(USER_NO),
+    PRIMARY KEY (ID)) CHARACTER SET utf8 ENGINE=InnoDB COLLATE utf8_general_ci";
+    mysqli_query($connect, $query) or die("");
+    
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 $query = "CREATE TABLE T_EVENT_INFO (
