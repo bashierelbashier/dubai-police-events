c17e5ee678672e4819033e34263ccd2ada583fa2
Basheir Elbashier <elbashier97@gmail.com>
2021-03-14 14:58:18 +0200

    [MOD] modify connect.php

--- MODEL/connect.php
+++ MODEL/connect.php
@@ -1,5 +1,5 @@
 <?php
 
-$connect = mysqli_connect("localhost", "bash", "kankan");
+$connect = mysqli_connect("localhost", "root", "");
 mysqli_select_db($connect, "EVENTS");
 mysqli_set_charset($connect, 'UTF8');
